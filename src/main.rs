55dec4dc30e7de8efe84669caf8f9427f5a56457
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2023-07-31 23:21:54 +0900

    [Modified] text ratio (seikinga)

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ fn make_seikin(text: &str, mut image: DynamicImage) -> DynamicImage {
     let mut optimizer: f32 = 0.0;
     
     if text.len() > standard_text_length {
-	optimizer = char_size * (text.chars().count() - standard_text_length) as f32;
+	optimizer += char_size * (text.chars().count() - standard_text_length) as f32;
 	scale.x -= optimizer;
 	scale.y -= optimizer;
     }
@@ -29,12 +29,12 @@ fn make_seikin(text: &str, mut image: DynamicImage) -> DynamicImage {
 
 fn make_seikinga(text: &str, mut image: DynamicImage) -> DynamicImage {
     //! "セイキンが" の文字の位置に 文字列 text を描画する
-    let font = Vec::from(include_bytes!("../static/MPLUS1-Bold.ttf") as &[u8]);
+    let font = Vec::from(include_bytes!("../static/MPLUS1-Black.ttf") as &[u8]);
     let font = Font::try_from_vec(font).unwrap();
     
     let mut scale = Scale {
-        x: 150.0,
-        y: 150.0,
+        x: 170.0,
+        y: 210.0,
     };
 
     let char_size: f32 = 25.0;
@@ -42,12 +42,12 @@ fn make_seikinga(text: &str, mut image: DynamicImage) -> DynamicImage {
     let mut optimizer: f32 = 0.0;
     
     if text.len() > standard_text_length {
-	optimizer = char_size * (text.chars().count() - standard_text_length) as f32;
+	optimizer += char_size * (text.chars().count() - standard_text_length) as f32;
 	scale.x -= optimizer;
 	scale.y -= optimizer;
-    }
+    } 
 
-    draw_text_mut(&mut image, Rgba([0u8, 0u8, 0u8, 0u8]), 950 + optimizer as u32, 750 + optimizer as u32, scale, &font, text);
+    draw_text_mut(&mut image, Rgba([0u8, 0u8, 0u8, 0u8]), 875 + optimizer as u32, 680 + optimizer as u32, scale, &font, text);
 
     return image;
 }
@@ -60,7 +60,7 @@ fn main() {
     let mut image = image::open("static/base.png").unwrap();
     
     let seikinga = "セイキンが";
-    let seikin = "Kyure_A";
+    let seikin = "SEIKIN";
 
     image = make_seikin(seikin, image.clone());
     image = make_seikinga(seikinga, image.clone());
