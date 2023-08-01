d6345a56591a620ca40fb80b2868bd1427f19830
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2023-08-01 23:29:46 +0900

    [Modified] optimizer

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,8 @@
-use image::{Rgba, DynamicImage, ImageBuffer, imageops::{resize, overlay}};
+use image::{Rgba, DynamicImage, ImageBuffer, imageops::{resize}};
 use imageproc::drawing::draw_text_mut;
 use rusttype::{Font, Scale};
 use og_image_writer::{style::BorderRadius};
 mod icon_round;
-
-fn make_seikin(text: &str, mut image: DynamicImage) -> DynamicImage {
-    //! "SEIKIN" の文字の位置に 文字列 text を描画する
-    let font = Vec::from(include_bytes!("../static/MPLUS1-Bold.ttf") as &[u8]);
-    let font = Font::try_from_vec(font).unwrap();
-    
-    let mut scale = Scale {
-        x: 320.0,
-        y: 320.0,
-    };
-
-    let char_size: f32 = 55.0;
-    let standard_text_length = 6; // "SEIKIN"
-    let mut optimizer: f32 = 0.0;
-    
-    if text.len() > standard_text_length {
-	optimizer += char_size * (text.chars().count() - standard_text_length) as f32;
-	scale.x -= optimizer;
-	scale.y -= optimizer;
-    }
-
-    draw_text_mut(&mut image, Rgba([0u8, 0u8, 0u8, 0u8]), 750 + optimizer as u32, 800 + optimizer as u32, scale, &font, text);
-
-    return image;
-}
 
 fn make_seikinga(text: &str, mut image: DynamicImage) -> DynamicImage {
     //! "セイキンが" の文字の位置に 文字列 text を描画する
@@ -43,13 +18,45 @@ fn make_seikinga(text: &str, mut image: DynamicImage) -> DynamicImage {
     let standard_text_length = 5; // "セイキンが"
     let mut optimizer: f32 = 0.0;
     
-    if text.len() > standard_text_length {
+    if text.chars().count() > standard_text_length {
 	optimizer += char_size * (text.chars().count() - standard_text_length) as f32;
 	scale.x -= optimizer;
 	scale.y -= optimizer;
     } 
 
     draw_text_mut(&mut image, Rgba([0u8, 0u8, 0u8, 0u8]), 875 + optimizer as u32, 680 + optimizer as u32, scale, &font, text);
+
+    return image;
+}
+
+fn make_seikin(text: &str, mut image: DynamicImage) -> DynamicImage {
+    //! "SEIKIN" の文字の位置に 文字列 text を描画する
+    let font = Vec::from(include_bytes!("../static/MPLUS1-Bold.ttf") as &[u8]);
+    let font = Font::try_from_vec(font).unwrap();
+    
+    let mut scale = Scale {
+        x: 320.0,
+        y: 300.0,
+    };
+
+    let mut pos = (750, 800);
+    
+    let char_size: f32 = 65.0;
+    let diff_size: u32 = 20;
+    let standard_text_length: u32 = 6; // "SEIKIN"
+    let mut char_optimizer: f32 = 0.0;
+    let mut pos_optimizer: u32 = 0;
+    
+    if text.chars().count() > standard_text_length as usize {
+	char_optimizer += char_size * (text.chars().count() as u32 - standard_text_length) as f32;
+	pos_optimizer += diff_size * (text.chars().count() as u32 - standard_text_length);
+	scale.x -= char_optimizer;
+	scale.y -= char_optimizer;
+	pos.0 += pos_optimizer;
+	pos.1 += pos_optimizer;
+    }
+
+    draw_text_mut(&mut image, Rgba([0u8, 0u8, 0u8, 0u8]), pos.0, pos.1, scale, &font, text);
 
     return image;
 }
@@ -74,14 +81,13 @@ fn draw_icon(mut image: DynamicImage, mut icon: DynamicImage) -> DynamicImage {
 
 fn main() {
     let mut image = image::open("static/base.png").unwrap();
-    let mut icon = image::open("static/identicon.png").unwrap();
+    let icon = image::open("static/identicon.png").unwrap();
     let seikinga = "キュレェが";
-    let seikin = "Kyure_A";
+    let seikin = "SEIKIn";
     
     image = make_seikin(seikin, image.clone());
     image = make_seikinga(seikinga, image.clone());
     image = draw_icon(image.clone(), icon.clone());
     
-    image.save("test.png").unwrap();	
-    icon.save("test2.png").unwrap();
+    image.save("result.png").unwrap();
 }
