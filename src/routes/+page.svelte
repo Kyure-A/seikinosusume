ad02cff3eaefaaef1373d104a670cf069617614c
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2023-08-02 16:39:17 +0900

    なんもわからん，雰囲気でやっている

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,44 @@
 <script lang="ts">
-  import generate from "../wasm/pkg/seikinosusume.js"
+  import { seikinosusume_generator } from "../wasm/pkg/seikinosusume";
   let seikinga: string = "セイキンが";
   let seikin: string = "SEIKIN";
   let files;
-  let path: string = "";
+
+  let canvasWidth = 0;
+  let canvasHeight = 0;
+
+  function generate (seikinga: string, seikin: string) {
+    const imageInput = document.getElementById('icon');
+    imageInput.addEventListener('change', (event) => {
+      const image = new Image();
+      image.onload = () => {
+        const { width, height } = image;
+        canvasWidth = width;
+        canvasHeight = height;
+
+        const canvas = document.getElementById('original-canvas');
+        canvas.width = width;
+        canvas.height = height;
+        canvas.getContext('2d').drawImage(image, 0, 0, width, height);
+        const { data } = canvas.getContext('2d').getImageData(0, 0, width, height);
+
+        const converted = seikinosusume_generator(data, width, height, seikinga, seikin);
+        const convertedImage = new ImageData(new Uint8ClampedArray(converted), width);
+        const convertedCanvas = document.getElementById('converted-canvas');
+        convertedCanvas.width = width;
+        convertedCanvas.height = height;
+        convertedCanvas.getContext('2d').putImageData(convertedImage, 0, 0, 0, 0, width, height);
+      };
+
+      const reader = new FileReader();
+      reader.onload = () => {
+        image.src = reader.result;
+      };
+
+      reader.readAsDataURL(event.target.files[0]);
+    });
+  } 
+  
 </script>
 
 <h1>セイキンおすすめシールジェネレーター</h1>
@@ -19,7 +54,10 @@
 <div><input bind:value={seikin} /></div>
 
 {#if files && files[0]}
-  <button on:click={generate(seikinga, seikin, files[0].name)}>Generate!</button>
+  <button on:click={generate(seikinga, seikin)}>Generate!</button>
 {/if}
+
+<canvas id="original-canvas" style="display: none;"></canvas>
+<canvas id="converted-canvas" style="margin-top: 20px;"></canvas>
 
 </div>
---
+++ src/wasm/src/seikinosusume_generator.rs
@@ -0,0 +1,96 @@
+use image::{Rgba, DynamicImage, ImageBuffer, imageops::{resize}};
+use imageproc::drawing::draw_text_mut;
+use rusttype::{Font, Scale};
+use og_image_writer::{style::BorderRadius};
+use crate::icon_round;
+
+fn make_seikinga(text: &str, mut image: DynamicImage) -> DynamicImage {
+    //! "セイキンが" の文字の位置に 文字列 text を描画する
+    let font = Vec::from(include_bytes!("../static/MPLUS1-Black.ttf") as &[u8]);
+    let font = Font::try_from_vec(font).unwrap();
+    
+    let mut scale = Scale {
+        x: 170.0,
+        y: 210.0,
+    };
+
+    let char_size: f32 = 25.0;
+    let standard_text_length = 5; // "セイキンが"
+    let mut optimizer: f32 = 0.0;
+    
+    if text.chars().count() > standard_text_length {
+	optimizer += char_size * (text.chars().count() - standard_text_length) as f32;
+	scale.x -= optimizer;
+	scale.y -= optimizer;
+    } 
+
+    draw_text_mut(&mut image, Rgba([0u8, 0u8, 0u8, 0u8]), 875 + optimizer as u32, 680 + optimizer as u32, scale, &font, text);
+
+    return image;
+}
+
+fn make_seikin(text: &str, mut image: DynamicImage) -> DynamicImage {
+    //! "SEIKIN" の文字の位置に 文字列 text を描画する
+    let font = Vec::from(include_bytes!("../static/MPLUS1-Bold.ttf") as &[u8]);
+    let font = Font::try_from_vec(font).unwrap();
+    
+    let mut scale = Scale {
+        x: 320.0,
+        y: 300.0,
+    };
+
+    let mut pos = (750, 800);
+    
+    let char_size: f32 = 65.0;
+    let diff_size: u32 = 20;
+    let standard_text_length: u32 = 6; // "SEIKIN"
+    let mut char_optimizer: f32 = 0.0;
+    let mut pos_optimizer: u32 = 0;
+    
+    if text.chars().count() > standard_text_length as usize {
+	char_optimizer += char_size * (text.chars().count() as u32 - standard_text_length) as f32;
+	pos_optimizer += diff_size * (text.chars().count() as u32 - standard_text_length);
+	scale.x -= char_optimizer;
+	scale.y -= char_optimizer;
+	pos.0 += pos_optimizer;
+	pos.1 += pos_optimizer;
+    }
+
+    draw_text_mut(&mut image, Rgba([0u8, 0u8, 0u8, 0u8]), pos.0, pos.1, scale, &font, text);
+
+    return image;
+}
+
+fn round_icon(icon: DynamicImage) -> DynamicImage {
+    //! icon を resize, 角丸 にする
+    let mut rounded_icon: ImageBuffer<Rgba<u8>, Vec<u8>> = icon.to_rgba8();
+    icon_round::round(&mut rounded_icon, &mut BorderRadius(200, 200, 200, 200));
+    let resized_icon: ImageBuffer<Rgba<u8>, Vec<u8>> = resize(&rounded_icon, 750, 750, image::imageops::FilterType::CatmullRom);
+
+    let new_icon: DynamicImage = DynamicImage::ImageRgba8(resized_icon);
+
+    return new_icon;
+}
+
+fn draw_icon(mut image: DynamicImage, mut icon: DynamicImage) -> DynamicImage {
+    //! icon を image にはめこむ
+    icon = round_icon(icon);
+    image::imageops::overlay(&mut image, &icon, 167, 200);
+    return image;
+}
+
+fn seikinosusume_generator(icon: ImageBuffer<Rgba<u8>, Vec<u8>>, seikinga: String, seikin: String) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
+    let mut image: DynamicImage = image::open("static/base.png").unwrap();
+    let dynamic_icon: DynamicImage = DynamicImage::ImageRgba8(icon);
+    
+    image = make_seikin(&seikin, image.clone());
+    image = make_seikinga(&seikinga, image.clone());
+    image = draw_icon(image.clone(), dynamic_icon.clone());
+    
+    return image.to_rgba8();
+}
+
+pub fn exec(buffer: Vec<u8>, width: u32, height: u32, seikinga: String, seikin: String) -> Vec<u8> {
+    let img = ImageBuffer::<Rgba<u8>, Vec<u8>>::from_raw(width, height, buffer).unwrap();
+    return seikinosusume_generator(img, seikinga, seikin).into_raw();
+}
